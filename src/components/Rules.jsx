bee69a2c2f44e514ca3f8ac2f3f5fe126c7f716a
V PAVAN <you@example.com>
2023-07-29 22:24:33 +0530

    Grammarcommit

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -8,8 +8,8 @@ const Rules = () => {
         <div>
         <p>Select any number</p>
         <p>Click on dice image</p>
-        <p>after click on  dice  if selected number is equal to dice number you will get same point as dice </p>
-        <p>if you get wrong guess then  2 point will be dedcuted </p>
+        <p>After clicking on  the  dice if selected number is equal to dice number then you will be awarded same point as dice </p>
+        <p>If you get wrong guess then  2 point will be dedcuted </p>
         </div>
     </RuleContainer>
   )
