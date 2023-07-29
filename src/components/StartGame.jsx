0d176c58f631cd62670a2d4b1d17c9f166230450
V PAVAN <you@example.com>
2023-07-29 22:19:15 +0530

    dicesimage

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -44,7 +44,7 @@ align-items: flex-end;
 const StartGame =({toggle})=>{
     return <Container>
         <div>
-        <img src='/images/dices.png' alt='dices'/></div>
+        <img src='./images/dices.png' alt='dices'/></div>
         <div className="context">
             <h1>DICE GAME</h1>
             <Button onClick={toggle}>Play Now</Button>
